cd8a77c3b76e36a8fc273eb581f85532394aa0d1
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-17 09:57:55 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
+//you can use this to paste in js code and have it run when index.html is
 
  let element = document.createElement('div');
 
