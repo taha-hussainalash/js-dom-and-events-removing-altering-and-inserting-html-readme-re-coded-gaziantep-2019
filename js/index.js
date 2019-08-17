d0311932bc53b9fcf661d8c45a0928bc5cd4ba84
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-17 09:55:15 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,12 +4,9 @@
 let element = document.createElement('div');
 element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
-
-
 document.body.appendChild(element);
 element.style.textAlign = 'center';
 let ul = document.createElement('ul');
- 
 for (let i = 0; i < 3; i++) {
   let li = document.createElement('li');
   li.innerHTML = (i + 1).toString();
