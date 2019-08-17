5f5431e2830d985e32b7a05d5359f0a69f80483c
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-17 09:53:28 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
-let element = document.createElement('div');element.innerHTML = 'Hello, DOM!';
+let element = document.createElement('div');
+element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
+
+
