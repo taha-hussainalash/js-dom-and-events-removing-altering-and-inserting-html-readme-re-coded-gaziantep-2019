35197a1f828b2b674b03185e965f740b17325784
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-17 09:54:31 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -17,4 +17,7 @@ for (let i = 0; i < 3; i++) {
  
 element.appendChild(ul);
 
-ul.style.textAlign = 'left';ul.removeChild(ul.querySelector('li:nth-child(2)'));
+ul.style.textAlign = 'left';
+
+
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
